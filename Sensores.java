8aa01fae1e13fbbe5b587b652ae793277b31bd67
xiongyuc <x.chen@uniandes.edu.co>
2026-04-18 10:19:18 -0500

    avanzes sensores

--- Sensores.java
+++ Sensores.java
@@ -1,12 +1,31 @@
-public class Sensores implements Runnable {
-    private int numB_sensores;
+import java.util.Random;
 
-    public Sensores(int numB_sensores) {
-        this.numB_sensores = numB_sensores;
+public class Sensores implements Runnable {
+    private int idSensor;
+    private int numBase_eventos;
+    private int ns;
+    private Random random = new Random();
+    private BuzonSemiactivo buzonEntrada;
+
+    public Sensores(int numBase_eventos, int idSensor, int numServidores, BuzonSemiactivo buzon) {
+        this.numBase_eventos = numBase_eventos;
+        this.idSensor = idSensor;
+        this.ns = numServidores;
+        this.buzonEntrada = buzon;
+
     }
 
     @Override
-    public void run() {}
+    public void run() {
+        int cantidadEventos = numBase_eventos * idSensor;
+
+        for (int i = 1; i <= cantidadEventos; i++) {
+            String idEvento = "S"+ idSensor + "-" + i;
+            int destino = random.nextInt(ns) + 1;
+            Evento evento = new Evento(idEvento, destino, false);
+            buzonEntrada.put(evento);
+        }
+    }
         
 
 }
